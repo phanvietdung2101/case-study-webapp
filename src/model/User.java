b00435fc0feb1a8078f8f302f2453e0a94a838bb
phanvietdung2101 <phanvietdung2101@gmail.com>
2020-04-15 16:03:57 +0700

    first commit

---
+++ src/model/User.java
@@ -0,0 +1,78 @@
+package model;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+    private String phone_number;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String email, String address, String phone_number, String password) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone_number = phone_number;
+        this.password = password;
+    }
+
+    public User(int id, String name, String email, String address, String phone_number, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone_number = phone_number;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
